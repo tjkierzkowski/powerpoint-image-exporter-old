0c01206abb30e72a37d162e4a8247ddc20796c0f
Tom Kierzkowski <52253+tjkierzkowski@users.noreply.github.com>
2020-09-01 12:28:22 -0400

    Refactored fixtures from unitests to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+from pathlib import Path
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pptx import Presentation
+
+from pptx_export.pptx_export import PowerPointImageExporter
+
+
+@pytest.fixture
+def fake_path(tmp_path):
+    fake_file_path = tmp_path / "presentations"
+    return fake_file_path
+
+
+@pytest.fixture
+def custom_path(tmp_path: Path) -> Path:
+    custom_path = tmp_path / "presentation_images"
+    return custom_path
+
+
+@pytest.fixture
+def default_path(tmp_path: Path) -> Path:
+    custom_path = tmp_path / "lecture_images"
+    return custom_path
+
+
+@pytest.fixture
+def fake_file():
+    return 'a_valid_file.pptx'
+
+
+@pytest.fixture
+def minimal_pres(tmp_path):
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+
+    title.text = "Hello, World!"
+    subtitle.text = "python-pptx was here!"
+    minimal_file = tmp_path / 'minimal.pptx'
+    prs.save(minimal_file)
+    return minimal_file
+
+
+@pytest.fixture
+def valid_presentation_name(custom_path, fake_file):
+    ppt_stub_file = custom_path / fake_file
+    pres = PowerPointImageExporter(ppt_stub_file)
+    return pres
