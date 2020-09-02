36e8921f10076c612e6c0a258b84a524880df248
Tom Kierzkowski <52253+tjkierzkowski@users.noreply.github.com>
2020-09-02 12:36:55 -0400

    - fixed session import issue with flake8-import-order

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ def tests(session):
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake-import-order")
+    session.install("flake8", "flake8-black", "flake8-import-order")
     session.run("flake8", *args)
 
 
